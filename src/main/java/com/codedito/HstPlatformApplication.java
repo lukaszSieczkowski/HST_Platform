eed48782804fb4b6256990fa327ed8a6942c4177
Łukasz Sieczkowski <sieczkowski.l@gmail.com>
2018-07-08 15:08:45 +0200

    initial commit

---
+++ src/main/java/com/codedito/HstPlatformApplication.java
@@ -0,0 +1,12 @@
+package com.codedito;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HstPlatformApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HstPlatformApplication.class, args);
+	}
+}
