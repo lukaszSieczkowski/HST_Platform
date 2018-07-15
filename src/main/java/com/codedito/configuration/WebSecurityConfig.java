7e1f55f6cc03afb89a218da3224a45fb4457ee94
Łukasz Sieczkowski <sieczkowski.l@gmail.com>
2018-07-15 15:05:04 +0200

    menu and login form added

--- src/main/java/com/codedito/configuration/WebSecurityConfig.java
+++ src/main/java/com/codedito/configuration/WebSecurityConfig.java
@@ -28,7 +28,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception {
 
         http.authorizeRequests()
-                .antMatchers("/", "/webjars/**", "/fragments/**", "/h2/**").permitAll()
+                .antMatchers("/", "/webjars/**", "/fragments/**", "/h2/**","/css/**").permitAll()
                 .antMatchers("/protected/user/**").hasRole("USER")
                 .antMatchers("/protected/admin/**").hasRole("ADMIN")
                 .anyRequest().authenticated()
